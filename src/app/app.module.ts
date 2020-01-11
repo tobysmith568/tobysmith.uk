4bac0b670848a58d47ff294513442c212ad42b6a
Toby Smith <tobysmith568@hotmail.co.uk>
2020-01-11 13:53:59 +0000

    Implemented the perfect scrollbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./components/home/home.component";
 import { Routes, RouterModule } from "@angular/router";
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { PerfectScrollbarConfigInterface, PERFECT_SCROLLBAR_CONFIG, PerfectScrollbarModule } from "ngx-perfect-scrollbar";
 
+const scrollConfig: PerfectScrollbarConfigInterface = {
+  suppressScrollX: true
+};
 
 const routes: Routes = [
   { path: "", pathMatch: "full", component: HomeComponent },
@@ -23,9 +26,17 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PerfectScrollbarModule
   ],
-  providers: [],
-  bootstrap: [ AppComponent ]
+  providers: [
+    {
+      provide: PERFECT_SCROLLBAR_CONFIG,
+      useValue: scrollConfig
+    }
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
