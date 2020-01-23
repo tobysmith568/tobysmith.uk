1108efe697c345cf931bf53b54e5d44428528d7b
Toby Smith <tobysmith568@hotmail.co.uk>
2020-01-23 23:20:48 +0000

    Fixed the dropdowns

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
+import { PerfectScrollbarComponent } from "ngx-perfect-scrollbar";
 
 @Component({
   selector: "app-root",
@@ -6,5 +7,9 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
+
+  @ViewChild(PerfectScrollbarComponent, { static: true })
+  public scrollbar: PerfectScrollbarComponent;
+
   title = "tobysmith-uk";
 }
