8bd9dd77c03e4eb1dd6802ae67d50c090ada4e13
Toby Smith <tobysmith568@hotmail.co.uk>
2020-01-26 14:12:36 +0000

    Started the home page

--- client/src/app/components/pages/home/home.component.ts
+++ client/src/app/components/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { PostService } from "src/app/services/posts/post.service";
+import { IPost } from "src/app/models/posts/post.interface";
 
 @Component({
   selector: "app-home",
@@ -7,9 +9,23 @@ import { Component, OnInit } from "@angular/core";
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public project: IPost;
+  public university: IPost;
+
+  constructor(private readonly postService: PostService) { }
 
   ngOnInit() {
+    const personalProjects = this.postService.getPostsInCategory("/projects", 1);
+
+    if (personalProjects.length > 0) {
+      this.project = personalProjects[0];
+    }
+
+    const universityProjects = this.postService.getPostsInCategory("/university", 1);
+
+    if (universityProjects.length > 0) {
+      this.university = universityProjects[0];
+    }
   }
 
 }
