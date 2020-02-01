2a9fce3bf1aeae8a5039d9a44d157c736ef747f8
Toby Smith <tobysmith568@hotmail.co.uk>
2020-02-01 20:43:57 +0000

    Small posts now navigate to their content

--- client/src/app/components/pages/home/small-post/small-post.component.ts
+++ client/src/app/components/pages/home/small-post/small-post.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { IPost } from "src/app/models/posts/post.interface";
+import { Router, ActivatedRoute } from "@angular/router";
+import { NewTabService } from "src/app/services/new-tab/new-tab.service";
 
 @Component({
   selector: "app-small-post",
@@ -11,9 +13,24 @@ export class SmallPostComponent implements OnInit {
   @Input()
   public post: IPost;
 
-  constructor() { }
+  constructor(private readonly router: Router,
+              private readonly activatedRoute: ActivatedRoute,
+              private readonly newTabService: NewTabService) { }
 
   ngOnInit() {
   }
 
+  public navigate() {
+    if (this.post.internalLink && !this.post.internalLink.isRelative) {
+      this.router.navigate([ this.post.internalLink.url ]);
+    } else if (this.post.internalLink && this.post.internalLink.isRelative) {
+      this.router.navigate([ this.post.internalLink.url ], { relativeTo: this.activatedRoute });
+    } else if (this.post.externalLink) {
+      this.newTabService.open(this.post.externalLink, true);
+    } else {
+      this.router.navigate([ this.post.slug ], {
+        relativeTo: this.activatedRoute
+      });
+    }
+  }
 }
