66f095c2e1d3d37807c24d1ab30fa88018b04f15
Toby Smith <tobysmith568@hotmail.co.uk>
2021-05-05 22:21:47 +0100

    Added Prettier

--- v2/tobysmith-uk/src/app/app-routing.module.ts
+++ v2/tobysmith-uk/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    initialNavigation: 'enabled'
-})],
+  imports: [
+    RouterModule.forRoot(routes, {
+      initialNavigation: "enabled"
+    })
+  ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
