8cf1e5ee29380dc97a1a10aec185d2355b01341b
Toby Smith <tobysmith568@hotmail.co.uk>
2021-05-09 16:15:54 +0100

    Created a components folder

--- v2/tobysmith-uk/src/main.server.ts
+++ v2/tobysmith-uk/src/main.server.ts
@@ -6,5 +6,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-export { AppServerModule } from "./app/app.server.module";
+export { AppServerModule } from "./app/components/app.server.module";
 export { renderModule, renderModuleFactory } from "@angular/platform-server";
