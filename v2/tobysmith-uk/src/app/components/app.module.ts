4a401eb35a7dc4d4bda521964e506bca7047c8f6
Toby Smith <tobysmith568@hotmail.co.uk>
2021-05-12 00:37:09 +0100

    Added caching of projects from server-side renders to client

--- v2/tobysmith-uk/src/app/components/app.module.ts
+++ v2/tobysmith-uk/src/app/components/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
+import { BrowserModule, BrowserTransferStateModule } from "@angular/platform-browser";
 import { NgScrollbarModule } from "ngx-scrollbar";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -29,7 +29,8 @@ import { environment } from "src/environments/environment";
     ContactModule,
     FourOhFourModule,
     GraphQLModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserTransferStateModule
   ],
   providers: [
     {
--- v2/tobysmith-uk/src/app/modules/graphql.module.ts
+++ v2/tobysmith-uk/src/app/modules/graphql.module.ts
@@ -1,22 +1,39 @@
-import { NgModule } from "@angular/core";
+import { InjectionToken, NgModule } from "@angular/core";
 import { APOLLO_OPTIONS } from "apollo-angular";
-import { ApolloClientOptions, InMemoryCache } from "@apollo/client/core";
+import { InMemoryCache } from "@apollo/client/core";
 import { HttpLink } from "apollo-angular/http";
 import { ENVIRONMENT, IEnvironment } from "src/environments/environment.interface";
+import { makeStateKey, TransferState } from "@angular/platform-browser";
 
-export function createApollo(httpLink: HttpLink, environment: IEnvironment): ApolloClientOptions<any> {
-  const link = httpLink.create({ uri: environment.apiUrl });
-  const cache = new InMemoryCache();
-
-  return { link: link as any, cache };
-}
+const APOLLO_CACHE = new InjectionToken<InMemoryCache>("apollo-cache");
+const STATE_KEY = makeStateKey<any>("apollo.state");
 
 @NgModule({
   providers: [
     {
+      provide: APOLLO_CACHE,
+      useValue: new InMemoryCache()
+    },
+    {
       provide: APOLLO_OPTIONS,
-      useFactory: createApollo,
-      deps: [HttpLink, ENVIRONMENT]
+      useFactory(httpLink: HttpLink, cache: InMemoryCache, transferState: TransferState, environment: IEnvironment) {
+        const isBrowser = transferState.hasKey<any>(STATE_KEY);
+
+        if (isBrowser) {
+          const state = transferState.get<any>(STATE_KEY, null);
+          cache.restore(state);
+        } else {
+          transferState.onSerialize(STATE_KEY, () => {
+            return cache.extract();
+          });
+        }
+
+        return {
+          link: httpLink.create({ uri: environment.apiUrl }),
+          cache
+        };
+      },
+      deps: [HttpLink, APOLLO_CACHE, TransferState, ENVIRONMENT]
     }
   ]
 })
