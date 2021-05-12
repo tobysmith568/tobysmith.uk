621febcd014af1521e4fe1d754d98395e92456e4
Toby Smith <tobysmith568@hotmail.co.uk>
2021-05-12 21:38:27 +0100

    Set up the search bar to show search terms on first load

--- v2/tobysmith-uk/src/app/components/general/header/search/search.component.ts
+++ v2/tobysmith-uk/src/app/components/general/header/search/search.component.ts
@@ -1,34 +1,48 @@
-import { Component, OnInit } from "@angular/core";
-import { NavigationEnd, Router, Event } from "@angular/router";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { NavigationEnd, Router, Event, ActivatedRoute, RoutesRecognized, ActivationEnd } from "@angular/router";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-search",
   templateUrl: "./search.component.html",
   styleUrls: ["./search.component.scss"]
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, OnDestroy {
   private readonly blogURL = "/blog";
   private readonly blogSearchURL = "/blog/search/";
+
+  private paramMapSubscription?: Subscription;
 
   public onBlog = false;
   public onBlogSearch = false;
 
+  public searchTerm = "";
+
   constructor(private readonly router: Router) {}
 
   ngOnInit(): void {
-    this.router.events.subscribe({
-      next: (event: Event) => {
-        if (!(event instanceof NavigationEnd)) {
-          return;
-        }
+    this.router.events.subscribe(event => {
+      if (event instanceof ActivationEnd) {
+        this.searchTerm = event.snapshot.paramMap.get("term") ?? "";
+      }
 
+      if (event instanceof NavigationEnd) {
         this.onBlog = event.url.startsWith(this.blogURL);
         this.onBlogSearch = event.url.startsWith(this.blogSearchURL);
       }
     });
   }
 
-  public async search(term: string): Promise<void> {
-    await this.router.navigate(["blog", "search", term]);
+  ngOnDestroy(): void {
+    this.paramMapSubscription?.unsubscribe();
+  }
+
+  public async search(): Promise<void> {
+    if (!this.searchTerm || this.searchTerm.length === 0) {
+      await this.router.navigate(["blog"]);
+      return;
+    }
+
+    await this.router.navigate(["blog", "search", this.searchTerm]);
   }
 }
