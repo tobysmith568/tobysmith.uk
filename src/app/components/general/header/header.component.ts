e55d2b46c3910abcf9f45cf2d3a8164d5395cecb
Toby Smith <tobysmith568@hotmail.co.uk>
2021-06-06 13:45:22 +0100

    Added the mobile title

--- src/app/components/general/header/header.component.ts
+++ src/app/components/general/header/header.component.ts
@@ -1,20 +1,42 @@
-import { AfterViewInit, Component, EventEmitter, Output, ViewChild } from "@angular/core";
+import { AfterViewInit, Component, EventEmitter, OnInit, Output, ViewChild } from "@angular/core";
+import { ActivatedRoute, NavigationEnd, Router } from "@angular/router";
 import { NgProgressComponent } from "ngx-progressbar";
 import { ProgressService } from "src/app/services/progress/progress.service";
+import { filter, map, mergeMap } from "rxjs/operators";
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
   styleUrls: ["./header.component.scss"]
 })
-export class HeaderComponent implements AfterViewInit {
+export class HeaderComponent implements OnInit, AfterViewInit {
+  public mobileTitle = "";
+
   @ViewChild(NgProgressComponent)
   public progressBar?: NgProgressComponent;
 
   @Output()
   public toggleMobileMenu: EventEmitter<void> = new EventEmitter<void>();
 
-  constructor(private readonly progresService: ProgressService) {}
+  constructor(private readonly router: Router, private readonly progresService: ProgressService) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe({
+      next: event => {
+        if (event instanceof NavigationEnd) {
+          const { url } = event;
+          const urlSegments = url.split("/");
+
+          if (urlSegments.length < 2 || urlSegments[1] === "") {
+            this.mobileTitle = "Toby Smith";
+            return;
+          }
+
+          this.mobileTitle = urlSegments[1];
+        }
+      }
+    });
+  }
 
   ngAfterViewInit(): void {
     this.progresService.$.subscribe(action => {
