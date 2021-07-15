80134484da27c9a30f6de098aa309e2d8af019c0
Toby Smith <tobysmith568@hotmail.co.uk>
2021-07-15 14:09:10 +0100

    Set devOnly posts to not show up in production

--- src/app/services/api/posts/posts.service.ts
+++ src/app/services/api/posts/posts.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from "@angular/core";
-import { gql, Query } from "apollo-angular";
+import { Inject, Injectable } from "@angular/core";
+import { Apollo, gql, Query } from "apollo-angular";
+import { ENVIRONMENT, IEnvironment } from "src/environments/environment.interface";
 import { SEO } from "../seo.interface";
 
 export interface Post {
@@ -18,6 +19,10 @@ interface Response {
   providedIn: "root"
 })
 export class PostsServiceGQL extends Query<Response> {
+  constructor(apollo: Apollo, @Inject(ENVIRONMENT) public readonly environment: IEnvironment) {
+    super(apollo);
+  }
+
   document = gql`
     query Posts {
       seo(where: { identifier: "posts" }) {
@@ -25,7 +30,7 @@ export class PostsServiceGQL extends Query<Response> {
         description
         noIndex
       }
-      posts(orderBy: date_DESC) {
+      posts(orderBy: date_DESC ${this.allowDevOnly()}) {
         slug
         title
         date
@@ -33,4 +38,12 @@ export class PostsServiceGQL extends Query<Response> {
       }
     }
   `;
+
+  private allowDevOnly(): string {
+    if (this.environment.production) {
+      return ", where: {OR: [{devOnly: null}, {devOnly: false}]}";
+    }
+
+    return "";
+  }
 }
