1c8a9d6f2fd428ee287ce2e828035fe58e1b4bd4
Toby Smith <tobysmith568@hotmail.co.uk>
2022-04-17 14:47:41 +0100

    Move the spaces in the recaptcha terms

--- src/components/contact/recaptcha-terms.tsx
+++ src/components/contact/recaptcha-terms.tsx
@@ -3,16 +3,14 @@ import styled from "@emotion/styled";
 const RecaptchaTerms = () => (
   <Wrapper>
     <span>
-      This site is protected by reCAPTCHA and the Google
+      This site is protected by reCAPTCHA and the Google{" "}
       <a href="https://policies.google.com/privacy" rel="noopener noreferrer" target="_blank">
-        {" "}
-        Privacy Policy{" "}
-      </a>
-      and
+        Privacy Policy
+      </a>{" "}
+      and{" "}
       <a href="https://policies.google.com/terms" rel="noopener noreferrer" target="_blank">
-        {" "}
-        Terms of Service apply{" "}
-      </a>
+        Terms of Service apply
+      </a>{" "}
       .
     </span>
   </Wrapper>
