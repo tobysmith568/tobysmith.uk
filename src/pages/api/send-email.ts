27717d885b429bc65aea2ae6318d0f757cd3b78b
Toby Smith <tobysmith568@hotmail.co.uk>
2022-04-17 01:55:10 +0100

    Migrate the send-email api to use zod

--- src/pages/api/send-email.ts
+++ src/pages/api/send-email.ts
@@ -1,22 +1,23 @@
 import { NextApiRequest } from "next";
 import { RouterBuilder } from "next-api-handler";
+import { z } from "zod";
 import { getEnv } from "../../utils/api-only/env";
 import { verifyRecaptchaToken } from "../../utils/api-only/recaptcha";
 import { sendPlainTextEmail } from "../../utils/api-only/send-email";
 
-export interface SubmitRequest {
-  name: string;
-  email: string;
-  message: string;
-  recaptchaToken: string;
-}
+const postRequestValidator = z.object({
+  name: z.string(),
+  email: z.string(),
+  message: z.string(),
+  recaptchaToken: z.string()
+});
 
-export interface SubmitResponse {
+export type SubmitResponse = {
   success: boolean;
-}
+};
 
 const postHandler = async (req: NextApiRequest) => {
-  const { name, email, message, recaptchaToken } = getPostBody(req);
+  const { name, email, message, recaptchaToken } = postRequestValidator.parse(req.body);
   const { from, to } = getEnv().email;
 
   const subject = `New message from ${name} via tobysmith.uk`;
@@ -25,28 +26,6 @@ const postHandler = async (req: NextApiRequest) => {
   await verifyRecaptchaToken(recaptchaToken);
 
   await sendPlainTextEmail(to, name, from, subject, fullMessage);
-};
-
-const getPostBody = (req: NextApiRequest): SubmitRequest => {
-  const body = req.body as Partial<SubmitRequest>;
-
-  if (!body?.name || typeof body.name !== "string") {
-    throw new Error("No name was given or it wasn't a string.");
-  }
-
-  if (!body?.email || typeof body.email !== "string") {
-    throw new Error("No email was given or it wasn't a string.");
-  }
-
-  if (!body?.message || typeof body.message !== "string") {
-    throw new Error("No message was given or it wasn't a string.");
-  }
-
-  if (!body?.recaptchaToken || typeof body.recaptchaToken !== "string") {
-    throw new Error("No recaptchaToken was given or it wasn't a string.");
-  }
-
-  return req.body;
 };
 
 const router = new RouterBuilder();
