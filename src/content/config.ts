0328e8eee0e379da7848b4a2cefee5ebe336cc86
Toby Smith <tobysmith568@hotmail.co.uk>
2024-02-04 23:36:50 +0000

    Add the projects page

---
+++ src/content/config.ts
@@ -0,0 +1,25 @@
+﻿import { defineCollection, z } from "astro:content";
+
+// Made a change to this file?
+// Run `npx astro sync` to update the type definitions
+
+const projectsCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    sortWeight: z.number(),
+    title: z.string(),
+    tagLine: z.string(),
+    description: z.string(),
+    image: z
+      .object({
+        url: z.string(),
+        alt: z.string()
+      })
+      .optional(),
+    tags: z.array(z.string()).optional()
+  })
+});
+
+export const collections = {
+  projects: projectsCollection
+};
