fd60dbdfb89de18c66862f0a5382b34312a5384b
Toby Smith <tobysmith568@hotmail.co.uk>
2024-02-04 14:51:43 +0000

    Switch to an empty Astro project

--- test/test-helpers/is-redirect-result.ts
+++
@@ -1,9 +0,0 @@
-import { GetServerSidePropsResult, Redirect } from "next";
-
-type RedirectResult = { redirect: Redirect };
-
-const isRedirectResult = <T>(result: GetServerSidePropsResult<T>): result is RedirectResult => {
-  return !!(result as RedirectResult).redirect;
-};
-
-export default isRedirectResult;
--- test/test-helpers/render-in-head.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from "@testing-library/react";
-
-const renderInHead = (component: JSX.Element) =>
-  render(component, {
-    container: document.head
-  });
-export default renderInHead;
